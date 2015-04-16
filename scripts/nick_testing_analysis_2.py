49da3f175b760a8c56e5a7385dc43a59631edb70
andrewosh <andrewosh@gmail.com>
2015-04-16 13:44:27 -0400

    Converting int to string

--- scripts/nick_testing_analysis_2.py
+++ scripts/nick_testing_analysis_2.py
@@ -63,7 +63,7 @@ class NicksAnalysis(AnalysisPipeline):
         #                    .toLightning(regression_viz, image_size, only_viz=True, plane=10)
 
         analysis1 = Analysis.SeriesBinnedRegressionAnalysis(input=self.dirs['input'], output=os.path.join(self.dirs['output'], 'weighted_mean'),
-                                                      prefix="m", format="binary", dims=str(dims), num_regressors=num_features,
+                                                      prefix="m", format="binary", dims=str(dims), num_regressors=str(num_features),
                                                       selected=str(num_selected), edges=str([x for x in xrange(0, 30, 2)]))\
                             .toImage(dims=tuple([num_selected + 2] + dims), preslice=slice(0, -num_features, 1))\
                             .colorize()\
